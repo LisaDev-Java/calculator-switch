af1e3b5aac7b84d1b1e91f313d313b403a29eae9
LisaDev-Java <pshenichnaya-01@list.ru>
2024-06-30 11:06:29 +0400

    added break

--- src/main/java/org/lisa/Lisa.java
+++ src/main/java/org/lisa/Lisa.java
@@ -9,8 +9,9 @@ public class Lisa {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input two number: ");
+        System.out.print("Input two number. First: ");
         double num1 = scanner.nextDouble();
+        System.out.print("Second: ");
         double num2 = scanner.nextDouble();
         System.out.print("Input a action (/ * - +) : ");
         String sign = scanner.next();
@@ -18,12 +19,16 @@ public class Lisa {
         switch (sign) {
             case "/":
                 System.out.println(num1 / num2);
+                break;
             case "*":
                 System.out.println(num1 * num2);
+                break;
             case "+":
                 System.out.println(num1 + num2);
+                break;
             case "-":
                 System.out.println(num1 - num2);
+                break;
         }
     }
-}                                                      // Переделать на Switch
+}
